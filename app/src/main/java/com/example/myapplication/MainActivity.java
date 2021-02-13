4276a349c413278de10070206df75e0751b85d10
= <MAdams6@uclan.ac.uk>
2021-02-13 15:29:56 +0000

    network request restructure

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.util.HashMap;
 
 // [REF: https://www.simplifiedcoding.net/android-mysql-tutorial-to-perform-basic-crud-operation/#Android-MySQL-Tutorial]
@@ -39,7 +43,55 @@ public class MainActivity extends AppCompatActivity {
         params.put("email", email);
         params.put("password", password);
 
-        NetworkRequest req = new NetworkRequest(this, API.URL_LOGIN, params, API.REQ_TYPE_POST);
+        LoginRequest req = new LoginRequest(API.URL_LOGIN, params, API.REQ_TYPE_POST);
         req.execute();
     }
+
+    private class LoginRequest extends AsyncTask<Void, Void, String> {
+        String url;
+        HashMap<String, String> params;
+        int requestCode;
+
+        LoginRequest(String url, HashMap<String, String> params, int requestCode)
+        {
+            this.url = url;
+            this.params = params;
+            this.requestCode = requestCode;
+        }
+
+        @Override
+        protected void onPreExecute() { }
+
+        @Override
+        protected void onPostExecute(String s)
+        {
+            try
+            {
+                JSONObject object = new JSONObject(s);
+                if (!object.getBoolean("error"))
+                {
+                    Toast.makeText(getApplicationContext(), object.getString("message"), Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... voids)
+        {
+            RequestHandler rqh = new RequestHandler();
+            if (requestCode == API.REQ_TYPE_POST)
+            {
+                return rqh.sendPostRequest(url, params);
+            }
+            if (requestCode == API.REQ_TYPE_GET)
+            {
+                return rqh.sendGetRequest(url);
+            }
+            return null;
+        }
+    }
+
 }
